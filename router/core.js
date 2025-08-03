5faeda4934c05095e6386c7d17347ea9f7a44d72
Augment Chinedu <augmentchinedu@gmail.com>
2025-08-03 06:50:41 +0100

    Docs link Added to Template

--- router/core.js
+++ router/core.js
@@ -5,7 +5,10 @@ const config = require("../config");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ text: `Welcome to ${config.appName} API` });
+  res.json({
+    text: `Welcome to ${config.appName} API`,
+    docs: `https://${req.headers.host}/docs`,
+  });
 });
 
 module.exports = router;
